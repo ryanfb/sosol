154e420145f1f64053a6aa1f8e1e9f7ec32e7164
Ryan Baumann <ryan.baumann@gmail.com>
2018-12-05 11:00:39 -0500

    Fix publication_id int cast migration for PostgreSQL

--- db/migrate/20091105164204_change_comments_publication_id_to_int.rb
+++ db/migrate/20091105164204_change_comments_publication_id_to_int.rb
@@ -1,6 +1,10 @@
 class ChangeCommentsPublicationIdToInt < ActiveRecord::Migration
   def self.up
-    change_column :comments, :publication_id, :integer
+    if ActiveRecord::Base.connection.adapter_name.downcase.include? 'postgresql'
+      change_column :comments, :publication_id, 'integer USING (publication_id::integer)'
+    else
+      change_column :comments, :publication_id, :integer
+    end
   end
 
   def self.down
